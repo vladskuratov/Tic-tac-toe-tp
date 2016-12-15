094843456f233fc46603397fe6a88c8c2c92ddf3
vladskuratov <vladskuratov@gmail.com>
2016-12-15 23:06:25 +0300

    additional ui

--- TicTacToe.Game/MainWindow.xaml.cs
+++ TicTacToe.Game/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace TicTacToe.Game
 {
@@ -25,22 +27,47 @@ namespace TicTacToe.Game
             gc.SwitchScreen += SwitchScreenImpl;
 
             contentControl.Content = ss;
-            contentControl.Margin = new Thickness { Left = 0 };
         }
 
-        public async void SwitchScreenImpl(ScreenType screen)
+        public void SwitchScreenImpl(ScreenType screen)
         {
-            contentControl.Margin = new Thickness{ Left = 0 };
+            int d, r;
 
-            if (screen == ScreenType.StartScreen) contentControl.Content = ss;
-            if (screen == ScreenType.GameWithFriend) contentControl.Content = gf;
-            if (screen == ScreenType.GameWithComputer) contentControl.Content = gc;
+            if (screen != ScreenType.StartScreen)
+            {
+                contentControl2.Margin = new Thickness { Left = 525 };
 
-            //for (int i = 400; i < 0; i--)
-            //{
-            //    //await Task.Delay(5);
-            //    contentControl.Margin = new Thickness { Left = i };
-            //}
+                if (screen == ScreenType.GameWithFriend) contentControl2.Content = gf;
+                if (screen == ScreenType.GameWithComputer) contentControl2.Content = gc;
+
+                d = 0;
+                r = 525;
+                var timer = new DispatcherTimer();
+                timer.Tick += delegate
+                {
+                    contentControl.Margin = new Thickness { Left = d -= 50 };
+                    contentControl2.Margin = new Thickness { Left = r -= 25 };
+                    if (contentControl2.Margin.Left == 0) timer.Stop();
+                };
+
+                timer.Interval = TimeSpan.FromMilliseconds(1);
+                timer.Start();
+            }
+            else
+            {
+                d = -1050;
+                r = 0;
+                var timer = new DispatcherTimer();
+                timer.Tick += delegate
+                {
+                    contentControl.Margin = new Thickness { Left = d += 50 };
+                    contentControl2.Margin = new Thickness { Left = r += 25 };
+                    if (contentControl.Margin.Left == 0) timer.Stop();
+                };
+
+                timer.Interval = TimeSpan.FromMilliseconds(1);
+                timer.Start();
+            }
         }
     }
 }
